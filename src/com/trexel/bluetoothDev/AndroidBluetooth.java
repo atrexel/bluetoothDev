ad0096c4b9802132db220f80b57b66f5d2a5db07
Amanda Trexel <atrexel1@gmail.com>
2014-10-06 01:40:33 -0700

    Implemented additional check if connection can be established

--- src/com/trexel/bluetoothDev/AndroidBluetooth.java
+++ src/com/trexel/bluetoothDev/AndroidBluetooth.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AndroidBluetooth extends Activity {
 
@@ -13,6 +14,9 @@ public class AndroidBluetooth extends Activity {
     private static final int REQUEST_PAIRED_DEVICE = 2;
 
     /** Called when the activity is first created. */
+    Button OnButton;
+    Button OffButton;
+    Button VisibleButton;
     Button btnListPairedDevices;
     TextView stateBluetooth;
     BluetoothAdapter bluetoothAdapter;
@@ -22,14 +26,19 @@ public class AndroidBluetooth extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        OnButton = (Button)findViewById(R.id.bluetoothOnButton);
+        OnButton.setOnClickListener(btOnButtonOnClickListener);
+        OffButton = (Button)findViewById(R.id.bluetoothOffButton);
+        OffButton.setOnClickListener(btOffButtonOnClickListener);
+        VisibleButton = (Button)findViewById(R.id.bluetoothVisibleButton);
+        VisibleButton.setOnClickListener(btVisibleButtonOnClickListener);
         btnListPairedDevices = (Button)findViewById(R.id.listpaireddevices);
+        btnListPairedDevices.setOnClickListener(btnListPairedDevicesOnClickListener);
 
         stateBluetooth = (TextView)findViewById(R.id.bluetoothstate);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         CheckBlueToothState();
-
-        btnListPairedDevices.setOnClickListener(btnListPairedDevicesOnClickListener);
     }
 
     private void CheckBlueToothState(){
@@ -43,7 +52,7 @@ public class AndroidBluetooth extends Activity {
                     stateBluetooth.setText("Bluetooth is Enabled.");
                     btnListPairedDevices.setEnabled(true);
                 }
-            }else{
+            } else {
                 stateBluetooth.setText("Bluetooth is NOT Enabled!");
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
@@ -53,14 +62,15 @@ public class AndroidBluetooth extends Activity {
 
     private Button.OnClickListener btnListPairedDevicesOnClickListener
             = new Button.OnClickListener(){
-
         @Override
         public void onClick(View arg0) {
             // TODO Auto-generated method stub
             Intent intent = new Intent();
             intent.setClass(AndroidBluetooth.this, ListPairedDevicesActivity.class);
             startActivityForResult(intent, REQUEST_PAIRED_DEVICE);
-        }};
+        }
+    };
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -73,4 +83,38 @@ public class AndroidBluetooth extends Activity {
             }
         }
     }
+
+    private Button.OnClickListener btOnButtonOnClickListener = new Button.OnClickListener(){
+        @Override
+        public void onClick(View arg0) {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnOn, 0);
+                Toast.makeText(getApplicationContext(),"Bluetooth Enabled"
+                        ,Toast.LENGTH_SHORT).show();
+                CheckBlueToothState();
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Bluetooth already Enabled",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+    private Button.OnClickListener btOffButtonOnClickListener = new Button.OnClickListener(){
+        @Override
+        public void onClick(View arg0) {
+            //turning bluetooth off this way requires BLUETOOTH_ADMIN permissions set in the AndroidManifest
+            bluetoothAdapter.disable();
+            Toast.makeText(getApplicationContext(),"Bluetooth Disabled" ,
+                    Toast.LENGTH_SHORT).show();
+            CheckBlueToothState();
+        }
+    };
+    private Button.OnClickListener btVisibleButtonOnClickListener = new Button.OnClickListener(){
+        @Override
+        public void onClick(View arg0) {
+            Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            startActivityForResult(getVisible, 0);
+        }
+    };
 }
--- src/com/trexel/bluetoothDev/ListPairedDevicesActivity.java
+++ src/com/trexel/bluetoothDev/ListPairedDevicesActivity.java
@@ -1,10 +1,14 @@
 package com.trexel.bluetoothDev;
+import java.util.UUID;
 import java.util.Set;
+import java.io.IOException;
 
+import android.util.Log;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListPairedDevicesActivity extends ListActivity {
+    String TAG = "BTLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +32,15 @@ public class ListPairedDevicesActivity extends ListActivity {
             for (BluetoothDevice device : pairedDevices) {
                 String deviceBTName = device.getName();
                 String deviceBTMAC = device.getAddress();
+                String deviceConnection = getBTConnectionStatus(device);
+                String deviceBTState = getBTBondState(device.getBondState());
                 String deviceBTMajorClass = getBTMajorDeviceClass(device
                                                 .getBluetoothClass()
                                                 .getMajorDeviceClass());
                 btArrayAdapter.add(deviceBTName + "\n"
                         + "Address: " + deviceBTMAC + "\n"
+                        + "State: " + deviceBTState + "\n"
+                        + "Connection: " + deviceConnection + "\n"
                         + "Class: " + deviceBTMajorClass);
             }
         }
@@ -39,6 +48,79 @@ public class ListPairedDevicesActivity extends ListActivity {
 
     }
 
+    //checks if the device is paired with the phone
+    private String getBTBondState(int bt_bond_state){
+        switch(bt_bond_state){
+            case android.bluetooth.BluetoothDevice.BOND_NONE:
+                return "Not Bonded";
+            case android.bluetooth.BluetoothDevice.BOND_BONDING:
+                return "Pairing Now";
+            case android.bluetooth.BluetoothDevice.BOND_BONDED:
+                return "Bonded";
+            default: return "unknown!";
+        }
+    }
+
+
+    //attempt to establish a connection via creating a bluetooth socket
+    private String getBTConnectionStatus(BluetoothDevice device){
+        // Use a temporary object that is later assigned to mmSocket,
+        // because mmSocket is final
+        BluetoothSocket tmpSocket = null;
+
+        // Default UUID
+        UUID DEFAULT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+        // Get a BluetoothSocket to connect with the given BluetoothDevice
+        try {
+            // Use the UUID of the device that discovered // TODO Maybe need extra device object
+            if (device != null)
+            {
+                Log.i(TAG, "Device UUID from fetch method: " + device.fetchUuidsWithSdp());
+                Log.i(TAG, "Device Name: " + device.getName());
+                Log.i(TAG, "Device UUID: " + device.getUuids()[0].getUuid());
+                tmpSocket = device.createRfcommSocketToServiceRecord(device.getUuids()[0].getUuid());
+
+                try {
+                    tmpSocket.connect();
+                    if(tmpSocket.isConnected()){
+                        tmpSocket.close();
+                        return "Could Connect";
+                    }
+                } catch (Exception e){
+                    Log.i(TAG, "Failed to make connection with Device UUID");
+                    return "Failed connection";
+                }
+            }
+            else Log.d(TAG, "Device is null.");
+        }
+        catch (NullPointerException e1)
+        {
+            Log.d(TAG, " UUID from device is null, Using Default UUID, Device name: " + device.getName());
+            try {
+                tmpSocket = device.createRfcommSocketToServiceRecord(DEFAULT_UUID);
+                try {
+                    tmpSocket.connect();
+                    if(tmpSocket.isConnected()){
+                        tmpSocket.close();
+                        return "Could Connect with Default";
+                    }
+                } catch (Exception e2){
+                    Log.i(TAG, "Failed to make connection with Default UUID");
+                }
+            } catch (IOException e3) {
+                Log.e(TAG, "IOException thrown [" + e3 + "]");
+                //e1.printStackTrace();
+                return "Failed connection";
+            }
+        }
+        catch (IOException e4) { return "Failed connection"; }
+
+        return "Failed connection";
+    }
+
+
+    //checks for the type of the device
     private String getBTMajorDeviceClass(int major){
         switch(major){
             case BluetoothClass.Device.Major.AUDIO_VIDEO:
@@ -62,7 +144,7 @@ public class ListPairedDevicesActivity extends ListActivity {
             case BluetoothClass.Device.Major.UNCATEGORIZED:
                 return "UNCATEGORIZED";
             case BluetoothClass.Device.Major.WEARABLE:
-                return "AUDIO_VIDEO";
+                return "WEARABLE_AUDIO_VIDEO";
             default: return "unknown!";
         }
     }
